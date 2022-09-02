f2296ed777070e70c8b47b370140a119918eb501
artur <artur@DESKTOP-V0JU5PP>
2022-09-02 15:39:18 -0300

    save

--- ProjetoFilaPessoas/Program.cs
+++ ProjetoFilaPessoas/Program.cs
@@ -8,17 +8,17 @@ namespace ProjetoFilaPessoas
         {
             FilaPessoas contatos = new FilaPessoas();
 
-            contatos.Print();
-            contatos.Pop();
-            contatos.Push(new Pessoa("morango", 32, "190"));
-            contatos.Push(new Pessoa("baratao", 47, "911"));
-            contatos.Print();
-            contatos.Length();
-            contatos.Pop();
-            contatos.Print();
-            contatos.Pop();
-            contatos.Print();
-            contatos.Length();
+            contatos.Print();//printa
+            contatos.Pop();//remove
+            contatos.Push(new Pessoa("morango", 32, "190"));//insere
+            contatos.Push(new Pessoa("baratao", 47, "911"));//insere
+            contatos.Print();//printa
+            contatos.Length();//mostra quantidade
+            contatos.Pop();//remove
+            contatos.Print();//printa
+            contatos.Pop();//remove
+            contatos.Print();//printa
+            contatos.Length();//mostra a quantidade
         }
     }
 }
